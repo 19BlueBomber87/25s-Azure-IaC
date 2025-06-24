aef0619e2d4dad798396da8943421451a771922b
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2025-06-24 13:36:42 -0800

    Update akWildConfig.sh

--- akWildConfig.sh
+++ akWildConfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+echo "wtf"
 
 # Set the home page.
-curl https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/21s-Web-HTML5-WildNet.html -o /var/www/html/index.html
+#sudo curl https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/21s-Web-HTML5-WildNet.html -o /var/www/html/index.html
