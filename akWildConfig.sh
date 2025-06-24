78181935f321a0b823fa62aa6ffe4fab5588ae69
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2025-06-24 11:54:48 -0800

    Update akWildConfig.sh
    remove for testing

--- akWildConfig.sh
+++ akWildConfig.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-# Update apt cache.
-sudo apt-get update
 
-#We need to add repo before installing nginx-core. Otherwise we get an error
-sudo add-apt-repository main
-sudo add-apt-repository universe
-sudo add-apt-repository restricted
-sudo add-apt-repository multiverse  
-
-# Install Nginx.
-sudo apt-get install -y nginx
 
 # Set the home page.
 sudo curl https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/21s-Web-HTML5-WildNet.html -o /var/www/html/index.html
