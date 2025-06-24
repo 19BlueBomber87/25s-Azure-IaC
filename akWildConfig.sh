45999f59ba3125255e0a0fe252c74148a7c6cb3d
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2025-06-23 17:02:21 -0800

    add akWildConfig.sh via upload
    Used for to configure Azure VMs running nginx.

---
+++ akWildConfig.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update apt cache.
+sudo apt-get update
+
+#We need to add repo before installing nginx-core. Otherwise we get an error
+sudo add-apt-repository main
+sudo add-apt-repository universe
+sudo add-apt-repository restricted
+sudo add-apt-repository multiverse  
+
+# Install Nginx.
+sudo apt-get install -y nginx
+
+# Set the home page.
+curl https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/21s-Web-HTML5-WildNet.html -o ./dude.html  /var/www/html/index.html
