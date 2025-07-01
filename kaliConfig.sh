c49a512474f557421bf806cf7a28a0a30ecaa8e9
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2025-07-01 11:34:28 -0800

    Update kaliConfig.sh

--- kaliConfig.sh
+++ kaliConfig.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+# Get Key before running apt-get update
+sudo wget https://archive.kali.org/archive-keyring.gpg -O /usr/share/keyrings/kali-archive-keyring.gpg
+
 # Update apt cache.
 sudo apt-get update
-
-# Get Key
-sudo wget https://archive.kali.org/archive-keyring.gpg -O /usr/share/keyrings/kali-archive-keyring.gpg
 
 #install Powershell
 sudo apt-get install -y powershell
