08f3f7f0da2d2af1b2b083452657a43bf9b5875b
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2025-07-01 16:46:49 -0800

    Update kaliConfig.sh

--- kaliConfig.sh
+++ kaliConfig.sh
@@ -5,14 +5,3 @@ sudo wget https://archive.kali.org/archive-keyring.gpg -O /usr/share/keyrings/ka
 
 # Update apt cache.
 sudo apt-get update
-
-#install Powershell
-sudo apt-get install -y powershell
-
-#create powershell script
-echo "Install-Module Cim -verbose *>&1" > yahoo.ps1
-echo "Install-Module -Name CYB3RTools-verbose *>&1" >> yahoo.ps1
-Write-Verbose -Message "$(uname -a)+ $(Get-Date)" -Verbose *>&1  > ComputerNameAndDate.ps1
-
-#Execute Script
-pwsh ./yahoo.ps1
