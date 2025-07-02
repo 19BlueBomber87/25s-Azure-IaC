d8b02748eda2486ed568ee6aa5cee68741341bca
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2025-07-02 15:57:50 -0800

    Update pic4web_IaC.ps1

--- pic4web_IaC.ps1
+++ pic4web_IaC.ps1
@@ -6,6 +6,9 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/toDoApp
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/jpg/bunny.jpg" -OutFile C:\\inetpub\\wwwroot\\jpg\\bunny.jpg
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/jpg/bunny2.jpg" -OutFile C:\\inetpub\\wwwroot\\jpg\bunny2.jpg
 
+mkdir C:\\inetpub\\wwwroot\\jpg
+
+
 #add computer info to web site
 $html = Get-Content C:\\inetpub\\wwwroot\\Default.htm
 $html.replace("Custom Heading Size and Font Type","$ENV:COMPUTERNAME`n$(Get-Date)`n$([System.Net.Dns]::GetHostAddresses($ENV:COMPUTERNAME))") | Out-File C:\\inetpub\\wwwroot\\Default.htm
