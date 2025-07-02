193b212a020b5f7968870db179d55ffe294d5f7e
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2025-07-02 13:47:06 -0800

    Add files via upload

---
+++ pic4web_IaC.ps1
@@ -0,0 +1,6 @@
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/25s-Azure-IaC/refs/heads/main/html/cert.jpg" -OutFile C:\\inetpub\\wwwroot\\jpg\\cert.jpg
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/jpg/AquaMoose.jpg" -OutFile C:\\inetpub\\wwwroot\\jpg\\AquaMoose.jpg
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/jpg/babymoose2.jpg" -OutFile C:\\inetpub\\wwwroot\\jpg\\babymoose2.jpg
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/jpg/bull.jpg" -OutFile C:\\inetpub\\wwwroot\\jpg\\bull.jpg
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/jpg/bunny.jpg" -OutFile C:\\inetpub\\wwwroot\\jpg\\bunny.jpg
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/jpg/bunny2.jpg" -OutFile C:\\inetpub\\wwwroot\\jpg\bunny2.jpg
