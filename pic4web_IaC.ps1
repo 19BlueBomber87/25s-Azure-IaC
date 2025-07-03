fcfe65b9dfd1504d7e016270ee0bb7f040955812
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2025-07-02 16:18:47 -0800

    Update pic4web_IaC.ps1

--- pic4web_IaC.ps1
+++ pic4web_IaC.ps1
@@ -1,3 +1,6 @@
+#HTML page pic directory
+mkdir C:\\inetpub\\wwwroot\\jpg
+
 #download Pics
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/25s-Azure-IaC/refs/heads/main/html/cert.jpg" -OutFile C:\\inetpub\\wwwroot\\jpg\\cert.jpg
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/jpg/AquaMoose.jpg" -OutFile C:\\inetpub\\wwwroot\\jpg\\AquaMoose.jpg
@@ -6,9 +9,6 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/toDoApp
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/jpg/bunny.jpg" -OutFile C:\\inetpub\\wwwroot\\jpg\\bunny.jpg
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/19BlueBomber87/toDoApp/refs/heads/master/jpg/bunny2.jpg" -OutFile C:\\inetpub\\wwwroot\\jpg\bunny2.jpg
 
-mkdir C:\\inetpub\\wwwroot\\jpg
-
-
 #add computer info to web site
 $html = Get-Content C:\\inetpub\\wwwroot\\Default.htm
-$html.replace("Custom Heading Size and Font Type","$ENV:COMPUTERNAME`n$(Get-Date)`n$([System.Net.Dns]::GetHostAddresses($ENV:COMPUTERNAME))") | Out-File C:\\inetpub\\wwwroot\\Default.htm
+$html.replace("Custom Heading Size and Font Type","$ENV:COMPUTERNAME`n$(Get-Date)`n$([System.Net.Dns]::GetHostAddresses($ENV:COMPUTERNAME))`n$($(Get-WmiObject win32_operatingsystem).caption)") | Out-File C:\\inetpub\\wwwroot\\Default.htm
