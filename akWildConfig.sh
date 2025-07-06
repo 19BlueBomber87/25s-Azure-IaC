d581afcf79f95b7d0f6278dc3761a24cf67eb094
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2025-07-05 18:58:34 -0800

    Update akWildConfig.sh

--- akWildConfig.sh
+++ akWildConfig.sh
@@ -17,15 +17,17 @@ sudo curl https://raw.githubusercontent.com/19BlueBomber87/25s-Azure-IaC/refs/he
 
 #Define characters
 host=$(hostname)
-opsys=$(uname -snm)
+opsys=$(uname -v)
 ip_address=$(ip a | grep 'inet ' | awk '{print $2}')
+$cpu=$(lscpu | grep 'Model name')
 host=$(hostname)
+
 today=$(date +%m-%d-%Y)
 
 #Fix illegal characters
 printIPvar=$(echo $ip_address | tr '/', '-')
 
-sudo sed -i "s/Custom Heading Size and Font Type/Welcome to Azure <br>Computer Name is-> $host<br>$opsys<br>$today<br>$printIPvar/g" /var/www/html/index.html
+sudo sed -i "s/Custom Heading Size and Font Type/Welcome to Azure <br>Computer Name: $host<br>OS Version: $opsys<br>Date: $today<br>CPU:$cpu<br>IP: $printIPvar/g" /var/www/html/index.html
 
 sudo mkdir /var/www/html/jpg
 
