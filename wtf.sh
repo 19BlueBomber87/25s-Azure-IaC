fdb0849f5ab759ad3911bdd746fe208a981872af
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2025-07-06 20:10:54 -0800

    Update wtf.sh

--- wtf.sh
+++ wtf.sh
@@ -5,11 +5,7 @@ opsys=$(uname -v)
 ip_address=$(ip a | grep 'inet ' | awk '{print $2}')
 cpu=$(lscpu | grep 'Model name')
 host=$(hostname)
-
 today=$(date +%m-%d-%Y)
-
 #Fix illegal characters
 printIPvar=$(echo $ip_address | tr '/', '-')
-
-
 sudo sed -i "s/Custom Heading Size and Font Type/Welcome to Azure <br>Computer Name: $host<br>OS Version: $opsys<br>Date: $today<br>CPU:$cpu<br>IP: $printIPvar/g" /var/www/html/index.html
