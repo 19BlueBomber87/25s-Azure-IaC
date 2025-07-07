b064f0224e6609b64ad92aeb2043fa5b5a0a4ef7
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2025-07-06 22:23:13 -0800

    Update akWildConfig.sh

--- akWildConfig.sh
+++ akWildConfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update apt cache.
-sudo apt-get -y update
+sudo apt-get update
 
 #We need to add repo before installing nginx-core. Otherwise we get an error
 sudo add-apt-repository -y main
