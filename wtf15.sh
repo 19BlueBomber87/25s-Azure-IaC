e3fe39b33c88a3f228224cdf9fca7ab757257236
[BlueBomber][MarkK.]@:D [Mark W. Kruse] <a19mkruse87k@outlook.com>
2025-07-06 21:37:47 -0800

    Update wtf15.sh

--- wtf15.sh
+++ wtf15.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo sed -i 's/'Custom Heading Size and Font Type'/'Fuckyou'/g' '/var/www/html/index.html'
+sudo sed -i "s/Custom Heading Size and Font Type/wtf/g" /var/www/html/index.html
